a24914a7e4007d074a09cd0b4f89472fdb7e7e99
KevDuc <kduconge@gmail.com>
2021-02-20 16:05:43 +0000

    Fix paths issues

--- UI/Item.js
+++ UI/Item.js
@@ -1,12 +1,12 @@
-import Port from "./Port.js";
-import Link from "./Link.js";
+import Port from './Port.js';
+import Link from './Link.js';
 
 export default class Item {
     constructor(canvas, type, position, center) {
         let img = document.createElement('img');
-        img.src = `/images/icons/${type}.svg`;
-        img.classList.add("item-img");
-        img.classList.add("disable-selection");
+        img.src = `./images/icons/${type}.svg`;
+        img.classList.add('item-img');
+        img.classList.add('disable-selection');
 
         let div = document.createElement('div');
         div.id = Item.generateId();
@@ -19,51 +19,49 @@ export default class Item {
         this.htmlElement = div;
         this.select();
 
-        this.htmlElement.addEventListener('dragstart', ev => {
+        this.htmlElement.addEventListener('dragstart', (ev) => {
             if (ev.target !== this.htmlElement.querySelector('.item-img')) return;
 
             let data = {
-                action: "move",
+                action: 'move',
                 itemId: ev.target.parentElement.id,
                 itemOffsetX: ev.offsetX,
-                itemOffsetY: ev.offsetY
+                itemOffsetY: ev.offsetY,
             };
 
-            ev.dataTransfer.setData("application/json", JSON.stringify(data));
+            ev.dataTransfer.setData('application/json', JSON.stringify(data));
         });
 
-        this.htmlElement.addEventListener('drop', ev => {
+        this.htmlElement.addEventListener('drop', (ev) => {
             if (!ev.target.classList.contains('item-img') && !ev.target.classList.contains('item')) return;
             ev.preventDefault();
 
-            let data = JSON.parse(ev.dataTransfer.getData("application/json"));
+            let data = JSON.parse(ev.dataTransfer.getData('application/json'));
 
             switch (data.action) {
                 case 'link':
                     let fromItem = document.querySelector(`#${data.itemId}`);
                     let toItem = this.htmlElement;
-                    if (fromItem !== toItem)
-                        new Link(fromItem.parentElement).connect(fromItem, toItem);
+                    if (fromItem !== toItem) new Link(fromItem.parentElement).connect(fromItem, toItem);
                     break;
                 default:
                     break;
             }
         });
 
-        this.htmlElement.addEventListener('click', ev => this.select());
+        this.htmlElement.addEventListener('click', (ev) => this.select());
 
-        this.htmlElement.addEventListener('select', ev => this.select());
+        this.htmlElement.addEventListener('select', (ev) => this.select());
 
-
-        document.addEventListener('keyup', ev => {
-            if (ev.key == "Delete" && this.selected) {
+        document.addEventListener('keyup', (ev) => {
+            if (ev.key == 'Delete' && this.selected) {
                 let binEvent = new Event('bin');
                 binEvent.item = this.htmlElement;
                 document.querySelector('#bin').dispatchEvent(binEvent);
             }
         });
 
-        document.addEventListener('mousedown', ev => this.deselect(), { useCapture: true });
+        document.addEventListener('mousedown', (ev) => this.deselect(), { useCapture: true });
 
         canvas.appendChild(this.htmlElement);
 
@@ -82,12 +80,12 @@ export default class Item {
 
     select() {
         this.selected = true;
-        this.htmlElement.style.borderColor = "lightskyblue";
+        this.htmlElement.style.borderColor = 'lightskyblue';
     }
 
     deselect() {
         this.selected = false;
-        this.htmlElement.style.borderColor = "rgba(255, 255, 255, 0)";
+        this.htmlElement.style.borderColor = 'rgba(255, 255, 255, 0)';
     }
 
     static N = 0;
