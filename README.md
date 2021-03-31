c1f1e794b34c6befa9a98eb29c0bf88553807e68
KevDuc <kduconge@gmail.com>
2021-03-31 19:10:55 +0100

    - Added favicon - Added internet element - Fixed ports always showing - Added bin closed not empty state - Added icons license - Fixed item position when dblclick on bin (put back to last position)

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # networks
+
 Create visual networks with common devices (routers, computers, etc.)  
 Try it here: http://kevduc.github.io/networks
 
 - Drag-n-drop the elements onto the canvas.
-- Each element has a port (blue circle) you can drag-n-drop to another element to create a connection
+- Each element has a port (blue circle when you hover over the element) that you can drag-n-drop onto other elements to create a connection  
+  <img src="https://user-images.githubusercontent.com/64347790/113188081-85385580-9251-11eb-8ac4-8aea4d3b51a9.png" title="port-example" height="64"/>
 
-![example](https://user-images.githubusercontent.com/64347790/108601820-88107280-7396-11eb-9e9f-0715ae8633c9.png)
+## Example
+
+<img src="https://user-images.githubusercontent.com/64347790/113187962-620da600-9251-11eb-897a-63ee6443db8e.png" title="example"/>
--- UI/Bin.js
+++ UI/Bin.js
@@ -1,60 +1,51 @@
 export default class Bin {
-    constructor(htmlElement) {
-        this.htmlElement = htmlElement;
-        this.content = [];
+  constructor(htmlElement) {
+    this.htmlElement = htmlElement
+    this.content = []
 
-        this.htmlElement.addEventListener('dragleave', ev => this.htmlElement.classList.remove('open'));
-        this.htmlElement.addEventListener('dragover', ev => {
-            ev.preventDefault();
-            this.htmlElement.classList.add('open');
-        });
-        this.htmlElement.addEventListener('drop', ev => {
-            ev.preventDefault();
+    this.htmlElement.addEventListener('dragleave', (ev) => this.htmlElement.classList.remove('open'))
+    this.htmlElement.addEventListener('dragover', (ev) => {
+      ev.preventDefault()
+      this.htmlElement.classList.add('open')
+    })
+    this.htmlElement.addEventListener(
+      'drop',
+      (ev) => {
+        ev.preventDefault()
 
-            let data = JSON.parse(ev.dataTransfer.getData("application/json"));
+        let data = JSON.parse(ev.dataTransfer.getData('application/json'))
 
-            switch (data.action) {
-                case "move":
-                    let item = document.querySelector(`#${data.itemId}`);
-                    this.bin(item);
-                    break;
-                default:
-                    break;
-            }
+        switch (data.action) {
+          case 'move':
+            let item = document.querySelector(`#${data.itemId}`)
+            this.bin(item)
+            break
+          default:
+            break
+        }
 
-            this.htmlElement.classList.remove('open')
-        }, { useCapture: true });
+        this.htmlElement.classList.remove('open')
+      },
+      { useCapture: true }
+    )
 
-        this.htmlElement.addEventListener('bin', ev => {
-            this.bin(ev.item);
-        });
+    this.htmlElement.addEventListener('bin', (ev) => {
+      this.bin(ev.item)
+    })
 
-        this.htmlElement.addEventListener('dblclick', ev => {
-            if (this.content.length == 0) return;
-            let item = this.content.pop();
-            if (this.content.length == 0) this.htmlElement.classList.replace('not-empty', 'empty');
+    this.htmlElement.addEventListener('dblclick', (ev) => {
+      if (this.content.length == 0) return
+      let item = this.content.pop()
+      if (this.content.length == 0) this.htmlElement.classList.replace('not-empty', 'empty')
 
-            let canvas = document.querySelector('#canvas');
-            let rect = canvas.getBoundingClientRect();
-            let position = { x: rect.width/2, y: rect.height/2};
+      item.style.display = ''
+    })
+  }
 
-            item.style.display = '';
-
-            item.style.left = `${position.x}px`
-            item.style.top = `${position.y}px`;
-            item.style.transform = `translate(-50%, -50%)`;
-
-            let rectItem = item.getBoundingClientRect();
-            item.style.left = `${rectItem.left}px`;
-            item.style.top = `${rectItem.top}px`;
-            item.style.transform = 'none';
-        })
-    }
-
-    bin(item) {
-        if (this.content.length == 0) this.htmlElement.classList.replace('empty', 'not-empty');
-        this.content.push(item);
-        item.dispatchEvent(new Event('remove'));
-        item.style.display = "none";
-    }
+  bin(item) {
+    if (this.content.length == 0) this.htmlElement.classList.replace('empty', 'not-empty')
+    this.content.push(item)
+    item.dispatchEvent(new Event('remove'))
+    item.style.display = 'none'
+  }
 }
